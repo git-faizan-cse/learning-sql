492b697e951aa631c3bc1e0e0939859dd8bfb82a
git-faizan-cse <mail.faizan.cse@gmail.com>
2026-01-16 04:43:22 +0530

    feat: implemented modify queries on table.

---
+++ alterTableScripts.sql
@@ -0,0 +1,6 @@
+-- this sql
+-- alter table users modify column email varchar(150) after id;
+-- alter table users modify column name varchar(100);
+-- alter table users modify column name varchar(150); after id;
+
+select * from users;
