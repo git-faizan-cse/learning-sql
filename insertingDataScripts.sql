c178838e55917eb7e3edb2904c0368b16d1e1eba
git-faizan-cse <mail.faizan.cse@gmail.com>
2026-01-16 05:03:02 +0530

    feat: implemented inserting data into tables also learned efficients ways to insert data.

---
+++ insertingDataScripts.sql
@@ -0,0 +1,14 @@
+-- insert into users values(
+-- 1, 'Alice', 'alice@gmail.com', 'female', '2015-07-23', default, default
+-- );
+-- insert into users (name, email, gender, date_of_birth) values
+-- ('faizan', 'faizan@gmail.com', 'Male', '2012-12-23');
+
+-- for multiple queries use: 
+insert into users ( name, email, gender, date_of_birth) values
+('bhavleen', 'bhavleen@gmail.com', 'Female', '2021-04-21'),
+('jyoti', 'jyoti@gmail.com', 'Female', '2011-03-10'),
+('wahid', 'wahid@gmail.com', 'Male', '2022-12-21'),
+('hardy', 'hardy@gmail.com', 'Male', '2012-11-12');
+
+select * from users;
