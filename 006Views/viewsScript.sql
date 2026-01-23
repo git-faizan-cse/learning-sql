d583aff37214a935544413d7355e7e15fa878585
git-faizan-cse <mail.faizan.cse@gmail.com>
2026-01-23 22:13:40 +0530

    feat(006Views): implemented Views in mysql

---
+++ 006Views/viewsScript.sql
@@ -0,0 +1,9 @@
+-- use startersql;
+
+-- CREATE VIEW rich_users AS
+-- select * from users Where salary > 70000;
+
+UPDATE users set salary = 30000 where id = 2; 
+select * from rich_users;
+
+select * from users; 
